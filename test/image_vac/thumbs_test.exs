f1bddb80c1e07b58d1a62ebc81ff74713b5a1827
Magnus Skog <magnus.m.skog@gmail.com>
2017-12-08 20:15:27 +0100

    Made tests simple and bumped watcher

--- test/image_vac/thumbs_test.exs
+++ test/image_vac/thumbs_test.exs
@@ -1,23 +1,27 @@
 defmodule ImageVac.ThumbsTest do
-  use ExUnit.Case
+  use ExUnit.Case, async: true
+
+  def thumbs_url do
+    Application.get_env(:image_vac, :thumbs_url)
+  end
 
   describe "image_url/1" do
     test "url" do
       url = "http://www.example.com/image.png"
-      assert ImageVac.Thumbs.image_url(url) == "#{Application.get_env(:image_vac, :thumbs_url)}/#{url}"
+      assert ImageVac.Thumbs.image_url(url) == "#{ImageVac.ThumbsTest.thumbs_url}/#{url}"
     end
   end
 
   describe "thumbnail_url/2" do
     test "with default size" do
       url = "http://www.example.com/image.png"
-      assert ImageVac.Thumbs.thumbnail_url(url) == "#{Application.get_env(:image_vac, :thumbs_url)}/x300/#{url}"
+      assert ImageVac.Thumbs.thumbnail_url(url) == "#{ImageVac.ThumbsTest.thumbs_url}/x300/#{url}"
     end
 
     test "with given size" do
       url = "http://www.example.com/image.png"
       size = 500
-      assert ImageVac.Thumbs.thumbnail_url(url, size) == "#{Application.get_env(:image_vac, :thumbs_url)}/x#{size}/#{url}"
+      assert ImageVac.Thumbs.thumbnail_url(url, size) == "#{ImageVac.ThumbsTest.thumbs_url}/x#{size}/#{url}"
     end
   end
 
